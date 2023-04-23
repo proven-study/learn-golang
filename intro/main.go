c80cf076487583640d44bf6c2395a700c69eb832
Md Arif Hossain <hi@devarif.me>
2023-04-23 21:13:43 +0600

    types

--- intro/main.go
+++ intro/main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	var x float64 = 20.0
+	fmt.Println(x)
+	fmt.Printf("x is of type %T\n", x)
+
+	// Late Initialization
+	var num int
+	var amount float32
+	var isValid bool
+	var name string
+	num = 20
+	amount = 49.99
+	isValid = true
+	name = "Bappy"
+	fmt.Println(num, amount, isValid, name)
 }
